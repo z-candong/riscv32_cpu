d7b8a1f4369f3a4f037ab52cc49ced4e5e930d73
zhengbh <123732683+z-candong@users.noreply.github.com>
2025-02-13 17:22:51 +0800

    Delete CODE/nemu/build directory

--- CODE/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.d
+++
@@ -1,9 +0,0 @@
-cmd_/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := unused
-
-source_/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := src/isa/riscv32/logo.c
-
-deps_/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o := \
-
-/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o: $(deps_/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o)
-
-$(deps_/home/zhengbh/Desktop/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/isa/riscv32/logo.o):
