49c8aabcf404aa4852304abe5cf475087db9a1bd
zhengbh <123732683+z-candong@users.noreply.github.com>
2025-02-14 17:31:24 +0800

    Update init.c

--- CODE/nemu/src/engine/interpreter/init.c
+++ CODE/nemu/src/engine/interpreter/init.c
@@ -14,14 +14,25 @@
 ***************************************************************************************/
 
 #include <cpu/cpu.h>
+// 包含CPU相关的头文件
 
 void sdb_mainloop();
+// 声明sdb_mainloop函数，该函数用于接收用户命令
 
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
+  // 如果定义了CONFIG_TARGET_AM宏，则执行以下代码
   cpu_exec(-1);
+  // 调用cpu_exec函数，传入参数-1，表示无限循环执行CPU指令
 #else
+  // 如果没有定义CONFIG_TARGET_AM宏，则执行以下代码
   /* Receive commands from user. */
+  // 接收用户命令
   sdb_mainloop();
+  // 调用sdb_mainloop函数，开始与用户的交互式会话
 #endif
 }
+
+
+
+
